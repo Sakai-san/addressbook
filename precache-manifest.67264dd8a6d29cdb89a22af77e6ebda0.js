35f45bfe697cf2d9ec9cd5c4404ebd47a06a40e6
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2020-07-30 23:27:54 +0200

    Updates

---
+++ precache-manifest.67264dd8a6d29cdb89a22af77e6ebda0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25f8d14755149179fe648949eafd93d6",
+    "url": "/addressbook/index.html"
+  },
+  {
+    "revision": "e90475da320ca7370993",
+    "url": "/addressbook/static/css/2.71916e70.chunk.css"
+  },
+  {
+    "revision": "c045ddbfecb9d00831e0",
+    "url": "/addressbook/static/css/main.3352f569.chunk.css"
+  },
+  {
+    "revision": "e90475da320ca7370993",
+    "url": "/addressbook/static/js/2.f77bdf8f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/addressbook/static/js/2.f77bdf8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c045ddbfecb9d00831e0",
+    "url": "/addressbook/static/js/main.226e64a0.chunk.js"
+  },
+  {
+    "revision": "36cac8a0a626b5b1de2a",
+    "url": "/addressbook/static/js/runtime-main.cf31af1a.js"
+  }
+]);
