d74db313d492e4f4bdd2e5eb85c81f42ca45658f
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2020-07-30 21:23:01 +0200

    Unit test.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+test("renders Settings link", () => {
+  const { getByText } = render(
+    <Provider
+      store={createStore(
+        () => ({
+          home: {
+            users: [],
+            fetching: false,
+          },
+          search: {
+            terms: null,
+          },
+          settings: false,
+        }),
+        applyMiddleware(thunk)
+      )}
+    >
+      <App />
+    </Provider>
+  );
+  const linkElement = getByText(/settings/i);
   expect(linkElement).toBeInTheDocument();
 });
