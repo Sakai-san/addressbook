d2eed224f915a8af980ad61dc72815b81d2a3f56
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2020-07-30 22:13:59 +0200

    Arrow function.

--- src/Home/operations.ts
+++ src/Home/operations.ts
@@ -12,9 +12,7 @@ const fetchUsers = (
     `https://randomuser.me/api/?nat=${nationality}&page=${page}&results=${amountOfRows}`
   )
     .then((r) => r.json())
-    .then((data) => {
-      dispatch(actions.makeUserFectch(data.results));
-    });
+    .then((data) => dispatch(actions.makeUserFectch(data.results)));
 };
 
 export default { fetchUsers };
