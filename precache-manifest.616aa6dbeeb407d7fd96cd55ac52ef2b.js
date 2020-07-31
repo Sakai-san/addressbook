e79489b15960f5a5194c8798ec1c2e2e20ff06f9
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2020-07-31 07:23:47 +0200

    Updates

--- precache-manifest.616aa6dbeeb407d7fd96cd55ac52ef2b.js
+++ precache-manifest.616aa6dbeeb407d7fd96cd55ac52ef2b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f04ff8ed4105a9458de46b8e69cb4c70",
+    "revision": "eb2f5c5959154ba38bf2ffb41d25855d",
     "url": "/addressbook/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/addressbook/static/css/2.71916e70.chunk.css"
   },
   {
-    "revision": "d11b698c443e118f0d4b",
+    "revision": "be965d5f8473810a3fc9",
     "url": "/addressbook/static/css/main.cf7a9322.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/addressbook/static/js/2.f77bdf8f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d11b698c443e118f0d4b",
-    "url": "/addressbook/static/js/main.74de853e.chunk.js"
+    "revision": "be965d5f8473810a3fc9",
+    "url": "/addressbook/static/js/main.55af1470.chunk.js"
   },
   {
     "revision": "36cac8a0a626b5b1de2a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/addressbook/precache-manifest.78e7043575fb10fcfc284d01ad0e1131.js"
+  "/addressbook/precache-manifest.616aa6dbeeb407d7fd96cd55ac52ef2b.js"
 );
 
 self.addEventListener('message', (event) => {
