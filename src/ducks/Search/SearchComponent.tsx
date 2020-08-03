4fd53935c1f0049ad19dc28270e0b07b9ab8c541
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2020-08-03 08:01:46 +0200

    Styling.

--- src/ducks/Search/SearchComponent.tsx
+++ src/ducks/Search/SearchComponent.tsx
@@ -1,12 +1,21 @@
 import React, { FunctionComponent, useRef, FormEvent, MouseEvent } from "react";
 import { useDispatch } from "react-redux";
 import actions from "./actions";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 import "./SearchComponent.css";
 
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
+
 const SearchComponent: FunctionComponent = () => {
-  const firstNameRef = useRef<HTMLInputElement>(null);
-  const lastNameRef = useRef<HTMLInputElement>(null);
+  const firstNameRef = useRef<HTMLDivElement | null>(null);
+  const lastNameRef = useRef<HTMLDivElement | null>(null);
 
   const dispatch = useDispatch();
 
@@ -15,8 +24,8 @@ const SearchComponent: FunctionComponent = () => {
 
     dispatch(
       actions.makeSearch({
-        first: firstNameRef?.current?.value,
-        last: lastNameRef?.current?.value,
+        first: firstNameRef?.current?.querySelector("input")?.value,
+        last: lastNameRef?.current?.querySelector("input")?.value,
       })
     );
   };
@@ -24,18 +33,45 @@ const SearchComponent: FunctionComponent = () => {
   const onResetHandler = (e: any | MouseEvent<HTMLElement>) =>
     dispatch(actions.makeSearch(null));
 
+  const classes = useStyles();
+
   return (
-    <form className="SearchComponent" onSubmit={onSubmitHandler}>
-      <div>
-        <input ref={firstNameRef} placeholder="first name" />
-      </div>
-      <div>
-        <input ref={lastNameRef} placeholder="last name" />
-      </div>
-      <button type="submit">Search</button>
-      <button onClick={onResetHandler} type="reset">
+    <form
+      className="SearchComponent"
+      onSubmit={onSubmitHandler}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField
+        ref={firstNameRef}
+        id="first-name"
+        label="First name"
+        className={classes.margin}
+      />
+      <TextField
+        ref={lastNameRef}
+        id="last-name"
+        label="Last name"
+        className={classes.margin}
+      />
+      <Button
+        size="medium"
+        color="primary"
+        variant="contained"
+        className={classes.margin}
+        type="submit"
+      >
+        Submit
+      </Button>
+      <Button
+        size="large"
+        variant="contained"
+        className={classes.margin}
+        onClick={onResetHandler}
+        type="reset"
+      >
         Reset
-      </button>
+      </Button>
     </form>
   );
 };
