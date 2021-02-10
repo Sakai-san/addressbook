5e80bad4b76010c58994e8e925b644901f701279
thomas <thomas@thomass-MacBook-Pro.local>
2021-02-10 22:33:19 +0100

    Debugging.

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
@@ -11,9 +12,25 @@ import * as serviceWorker from "./serviceWorker";
 const composeEnhancers =
   (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const log1 = (store) => (next) => (action) => {
+  console.log(action);
+
+  next({ ...action, log: 1 });
+};
+
+const log2 = (store) => (next) => (action) => {
+  console.log(action);
+  next({ ...action, log: 2 });
+};
+
+const log3 = (store) => (next) => (action) => {
+  console.log(action);
+  next({ ...action, log: 3 });
+};
+
 const store = createStore(
   combineReducers(ducks),
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(thunk, log1, log2, log3))
 );
 
 ReactDOM.render(
